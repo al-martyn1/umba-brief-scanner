eb0b2a9237134a9d297d7660861a065c286f6fa0
al-martyn1 <amart@mail.ru>
2024-08-24 01:11:05 +0300

    Добавил нужного, удалил ненужное

--- _generate_msvc.bat
+++
@@ -1,5 +0,0 @@
-@rem Генерация сборочных скриптов x86/x64 Debug/Release для заданной версии MSVC
-@rem call this: call _generate_msvc.bat msvc2019|msvc2022
-@rem https://learn.microsoft.com/en-us/cpp/build/customize-cmake-settings?view=msvc-170
-@call %~dp0\.bat\_generate_msvc.bat %1 && @call %~dp0\.bat\_generate_msvc.bat %1
-
